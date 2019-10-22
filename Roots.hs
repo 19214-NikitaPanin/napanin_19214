b5a1b09d1bbc0ea74f05c649892fe782f48d6504
19214-NikitaPanin <55292475+19214-NikitaPanin@users.noreply.github.com>
2019-10-22 21:42:55 +0700

    Add files via upload

---
+++ Roots.hs
@@ -0,0 +1,9 @@
+roots a b c =  if a == 0 && c == 0 && b == 0 then  error "a=0, b=0, c=0, So...What to do?"
+					else if a == 0 && b == 0 then c
+						else if a == 0 then ((-1)*c) / b 
+							else 
+								if d then (x1, x2) where
+											d = b*b - 4 * a * c
+											x1 = (-b + sqrt d) / 2
+											x2 = (-b - sqrt d) / 2
+							    else = error "Diskrimonant < 0"
