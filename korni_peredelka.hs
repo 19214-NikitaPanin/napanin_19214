2c7fe20d78124977bf11838a3b842f643fadb284
19214-NikitaPanin <55292475+19214-NikitaPanin@users.noreply.github.com>
2019-10-22 21:43:14 +0700

    Delete korni_peredelka.hs

--- korni_peredelka.hs
+++
@@ -1,7 +0,0 @@
-roots a b c  | a == 0 = case b of 0 -> error "Ento ne to chto kvadratnoe, dazhe ne lineinoe uravnenie"
-                                  _ -> ((-1)*c) / b
-             | d >= 0 = (x1, x2)
-             |otherwise = error "Diskrimonant < 0"			 where
-                                     d = b*b - 4 * a * c
-                                     x1 = (-b + sqrt d) / 2
-                                     x2 = (-b - sqrt d) / 2
