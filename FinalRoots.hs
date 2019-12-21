5618f65246a1e5df65518d47cb6c0751b6f298fe
19214-NikitaPanin <55292475+19214-NikitaPanin@users.noreply.github.com>
2019-12-22 00:42:31 +0700

    Update FinalRoots.hs

--- FinalRoots.hs
+++ FinalRoots.hs
@@ -1,9 +1,10 @@
-roots a b c =  if a == 0 && c == 0 && b == 0 then  error "a=0, b=0, c=0, So...What to do?"
-					else if a == 0 && b == 0 then c
-						else if a == 0 then ((-1)*c) / b 
-							else 
-								if d>=0 then (x1, x2) where
-											d = b*b - 4 * a * c
-											x1 = (-b + sqrt d) / 2
-											x2 = (-b - sqrt d) / 2
-							    else  error "Diskrimonant < 0"
+roots 0 0 0 = error "Nothing to calculate"
+roots 0 0 _ = error "No Solutions"
+roots a b c 	| a == 0 = (x, x)
+		| d == 0 = (x, x)
+		| a /= 0 = if d<0 then error "No Solutions" else (x1, x2)
+										where
+											x = -b / (2*a)
+											x1 = (-b + sqrt d ) / (2*a)
+											x2 = (-b - sqrt d ) / (2*a)
+											d = b*b - 4*a*c
